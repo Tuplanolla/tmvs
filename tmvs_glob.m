38f5486831ae5c9836c0f2bf9d6bb4d48fe72e68
Sampsa Kiiskinen <tuplanolla@gmail.com>
2016-06-23 01:08:20 +0300

    Most horrifying regular expression parser.

---
+++ tmvs_glob.m
@@ -0,0 +1,10 @@
+function arrays = tmvs_glob(pattern)
+filenames = glob(pattern);
+
+arrays = struct();
+for i = 1 : length(filenames)
+  filename = filenames{i};
+
+  arrays = tmvs_merge(arrays, tmvs_fetch(filename));
+end
+end
---
+++ tmvs_identify.m
@@ -0,0 +1,76 @@
+function y = tmvs_identify(str)
+% TODO Ha ha! Regular expressions!
+pat = ['^(KoeRak(?<site>[A-Z])(?<level>L|S)(?<room1>[0-9]+)( Meta)? - ((?<quantity1>T|RH|AH)(?<room2>[0-9]+) (?<placement>L(at)?|A|Y)(?<number>[0-9]+) (?<depth>[0-9]+)mm.*|lisa140)|', ...
+       '(?<region>Autiolah|Jyv)[^-]* - [^ ]*(?<quantity2>tila|kosteus|paine|tuul|sade).*)$'];
+[~, ~, ~, ~, ~, nm] = regexpi(str, pat);
+
+if ~isempty(nm.quantity1)
+  source = 'test lab';
+
+  switch tolower(nm.quantity1)
+  case 'T'
+    quantity = 'temperature';
+  case 'RH'
+    quantity = 'relative humidity';
+  case 'AH'
+    quantity = 'absolute humidity';
+  end
+
+  site = nm.site;
+
+  if nm.room1 ~= nm.room2
+    error('malformed identifier');
+  end
+  room = nm.room1;
+
+  depth = str2double(nm.depth);
+
+  switch tolower(nm.placement)
+  case {'L', 'Lat'}
+    placement = 'level floor';
+  case 'A'
+    placement = 'wall bottom corner';
+  case 'Y'
+    placement = 'wall top corner';
+  end
+
+  % TODO This.
+  material = 'mineral wool';
+
+  y = struct('source', tmvs_source(source), ...
+             'quantity', tmvs_quantity(quantity), ...
+             'site', tmvs_site(site), ...
+             'room', tmvs_room(room), ...
+             'depth', depth, ...
+             'placement', tmvs_placement(placement), ...
+             'material', tmvs_material(material));
+elseif ~isempty(nm.quantity2)
+  source = 'small weather station';
+
+  switch tolower(nm.quantity2)
+  case 'tila'
+    quantity = 'temperature';
+  case 'kosteus'
+    quantity = 'relative humidity';
+  case 'paine'
+    quantity = 'pressure';
+  case 'tuul'
+    quantity = 'wind speed';
+  case 'sade'
+    quantity = 'precipitation';
+  end
+
+  switch tolower(nm.region)
+  case 'autiolah'
+    region = 'Autiolahti';
+  case 'jyv'
+    region = 'Jyvaskyla';
+  end
+
+  y = struct('source', tmvs_source(source), ...
+             'quantity', tmvs_quantity(quantity), ...
+             'region', tmvs_region(region));
+else
+  error('malformed identifier');
+end
+end
