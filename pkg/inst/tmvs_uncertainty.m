3bfdb30f03bb29ef355dfacc288c0b90be378820
Sampsa Kiiskinen <tuplanolla@gmail.com>
2016-07-02 17:20:56 +0300

    Fix package things.

--- pkg/inst/tmvs_uncertainty.m
+++ pkg/inst/tmvs_uncertainty.m
@@ -2,20 +2,20 @@ function delta = tmvs_uncertainty (id, value = nan)
 
 % TODO Replace these educated guesses with accurate data.
 switch tmvs_quantity (id.quantity)
-  case 'temperature'
-    delta = 1;
-  case 'relative humidity'
-    delta = 10;
-  case 'absolute humidity'
-    delta = 1;
-  case 'pressure'
-    delta = 1;
-  case 'wind speed'
-    delta = 0.1;
-  case 'precipitation'
-    delta = 1;
-  otherwise
-    error (sprintf ('unknown quantity %d', id.quantity));
-case
+case 'temperature'
+  delta = 1;
+case 'relative humidity'
+  delta = 10;
+case 'absolute humidity'
+  delta = 1;
+case 'pressure'
+  delta = 1;
+case 'wind speed'
+  delta = 0.1;
+case 'precipitation'
+  delta = 1;
+otherwise
+  error (sprintf ('unknown quantity %d', id.quantity));
+end
 
 end
