38f2b125f2e68cf4914cec7274138d05ca3a0f59
Sampsa Kiiskinen <tuplanolla@gmail.com>
2016-07-02 17:28:48 +0300

    Merging needs some work.

--- pkg/inst/tmvs_merge.m
+++ pkg/inst/tmvs_merge.m
@@ -1,11 +1,37 @@
+% TODO This is shit.
+
 function a = tmvs_merge (a1, a2)
+
+a = a1;
 
 n = rows (a2);
 
 for i = 1 : n
-  a1 = tmvs_insert (a1, a2(i).id, a2(i).pairs);
+  id = a2(i).id;
+  meta = a2(i).meta;
+  pairs = a2(i).pairs;
+
+  z = 1;
+  new = true;
+  for z = 1 : length (a)
+    if isequaln (a(z).id, id)
+      new = false;
+
+      break
+    end
+  end
+
+  if new
+    a(end + 1) = struct ('id', id, 'meta', meta, 'pairs', pairs);
+  else
+    a(z).pairs = [a(z).pairs; pairs];
+  end
 end
 
-a = a1;
+k = 1;
+for i = 1 : length (a)
+  [y, j] = unique (a(i).pairs(:, 1));
+  a(i).pairs = a(i).pairs(j, :);
+end
 
 end
